5ecc8c57c356345351ae78b2462a746e9952a82e
Marcos de Lima Bühl <marcoslimabuhl@gmail.com>
2023-03-19 03:55:41 -0300

    Adicionado exercicios2-6.py

---
+++ exerciciosSemana3/exercicios2-6.py
@@ -0,0 +1,10 @@
+# Primeiro, execute a atribuição
+# palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+
+palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+print(palavras)
+
+# Agora, escreva duas expressões Python que são avaliadas, respectivamente,
+# como a primeira e a última palavras em palavras, na ordem do dicionário.
+print(palavras[0])
+print(palavras[-1])
