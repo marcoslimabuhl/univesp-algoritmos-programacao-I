7a4cb0e942a56bf250675c9ea48f179fdf9510ac
Marcos de Lima Bühl <marcoslimabuhl@gmail.com>
2023-09-11 23:27:17 -0300

    adicionando_letras_separação

--- exerciciosSemana3/exercicios2-9.py
+++ exerciciosSemana3/exercicios2-9.py
@@ -1,12 +1,12 @@
 # Qual é o tipo do objeto ao qual essas expressões são avaliadas?
 # (a) False + False
-print(type(False + False))
+print("a)", type(False + False))
 
 # (b) 2 * 3**2.0
-print(type(2 * 3**2.0))
+print('b)',type(2 * 3**2.0))
 
 # (c) 4 // 2 + 4 % 2
-print(type(4 // 2 + 4 % 2))
+print('c)',type(4 // 2 + 4 % 2))
 
 # (d) 2 + 3 == 4 or 5 >= 5
-print(type(2 + 3 == 4 or 5 >= 5))
+print('d)',type(2 + 3 == 4 or 5 >= 5))
